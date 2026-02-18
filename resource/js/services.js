aa602a75f3718505d19e6e4afa0559884ef1694f
Miguel Enrique Martinez <miguel.enrique@Quipux.local>
2026-02-18 08:07:28 -0500

    servicios

---
+++ resource/js/services.js
@@ -0,0 +1,76 @@
+angular.module("FinalApp")
+.factory("PostService", function() {
+    var posts = [
+        {
+            id: 1,
+            title: "Bienvenido a Posts App",
+            content: "Esta es una aplicación de ejemplo creada con AngularJS. Puedes crear, ver y eliminar posts.",
+            author: "Admin",
+            date: new Date(),
+            likes: 5
+        },
+        {
+            id: 2,
+            title: "AngularJS es genial",
+            content: "AngularJS nos permite crear aplicaciones web dinámicas de manera sencilla con su sistema de data binding bidireccional.",
+            author: "Developer",
+            date: new Date(),
+            likes: 10
+        }
+    ];
+    
+    var nextId = 3;
+    
+    return {
+        getPosts: function() {
+            return posts;
+        },
+        
+        getPost: function(id) {
+            for (var i = 0; i < posts.length; i++) {
+                if (posts[i].id == id) {
+                    return posts[i];
+                }
+            }
+            return null;
+        },
+        
+        addPost: function(post) {
+            post.id = nextId++;
+            post.date = new Date();
+            post.likes = 0;
+            posts.unshift(post);
+            return post;
+        },
+        
+        deletePost: function(id) {
+            for (var i = 0; i < posts.length; i++) {
+                if (posts[i].id == id) {
+                    posts.splice(i, 1);
+                    return true;
+                }
+            }
+            return false;
+        },
+        
+        likePost: function(id) {
+            var post = this.getPost(id);
+            if (post) {
+                post.likes++;
+                return post.likes;
+            }
+            return 0;
+        },
+        
+        updatePost: function(id, updatedData) {
+            var post = this.getPost(id);
+            if (post) {
+                post.title = updatedData.title;
+                post.content = updatedData.content;
+                post.author = updatedData.author;
+                return post;
+            }
+            return null;
+        }
+    };
+});
